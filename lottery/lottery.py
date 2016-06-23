f86895585eae5cf17f9b2f8f2bd0cd101a7d4282
albert <albert@Erek>
2016-06-23 16:17:30 +0200

    first version of the script. All terminal, asks for a route to find a txt file and a number.

--- lottery/lottery.py
+++ lottery/lottery.py
@@ -1,6 +1,28 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+import os, random
+
+def pick_winner(file):
+    lines = []
+    with open(file, "r") as f:
+        lines = f.readlines();
+    random_line_num = random.randrange(0, len(lines))
+    return lines[random_line_num]
 
 if __name__ == "__main__":
-    print("Hello World")
+    print("Route to the file:")
+    fil = raw_input()
+    print("Number of Winners:")
+    numb = int(raw_input())
+    while numb!= 0 :
+        win = pick_winner(fil)
+        if numb == 3:
+            ordinal='rd'
+        elif numb == 1:
+            ordinal = 'st'
+        elif numb == 2:
+            ordinal = 'nd'
+        elif numb > 3:
+            ordinal = 'th'
+        print(str(numb) + ordinal + ' place: ' + win)
+        numb = numb - 1
+    print("End lottery. Enjoy!")
+        
